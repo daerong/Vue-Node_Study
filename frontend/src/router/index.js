8c9a2a8c69c0cfefdf9d2fcbeed7c2e0fc4c3814
daerong <yu5501@nate.com>
2020-12-18 17:38:49 +0900

    Practice #1 Success. - 영화 목록, 상세페이지 완료

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,37 @@
+/* eslint-disable */
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import MovieListPage from '@/components/MovieListPage'
+import DetailMoviePage from '@/components/DetailMoviePage'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'movielist',
+      component: MovieListPage
+    },
+    {
+      path: '/:id',
+      name: 'detailmovie',
+      component: DetailMoviePage
     }
   ]
 })
+
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+
+// Vue.use(Router)
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     }
+//   ]
+// })
